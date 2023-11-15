f038bf5706de9442415f795fb4a9266259a57dc7
MyllysMikko <s2200860@edu.bc.fi>
2023-11-15 14:37:03 +0200

    Kenttä piirretään

--- Gravity/Game.cs
+++ Gravity/Game.cs
@@ -13,9 +13,13 @@ namespace Gravity
     {
         int window_width = 960;
         int window_height = 720;
+
+
         Player player;
         
         TiledMap map;
+
+        List<Texture> tileSets = new List<Texture>();
 
         Stack<GameState> gameStates = new Stack<GameState>();
 
@@ -44,10 +48,17 @@ namespace Gravity
             Vector2 playerPos = new Vector2(window_width * 0.5f, window_height * 0.5f);
             player.transform.position = playerPos;
 
-            map = MapReader.LoadMapFromFile("Map/testmap.tmj");
+            map = MapReader.LoadMapFromFile("Map/testmap2.tmj");
+            foreach (var tilesetFile in map.tilesetFiles)
+            {
+                string fileName = tilesetFile.imageWoPath;
+                Texture texture = Raylib.LoadTexture($"Map/Tilesets/{fileName}");
+                tileSets.Add(texture);
+            }
             if (map != null)
             {
                 Console.WriteLine("Map loaded!");
+                map.PrintToConsole();
             }
         }
 
@@ -93,25 +104,45 @@ namespace Gravity
         {
             Raylib.BeginDrawing();
             Raylib.ClearBackground(Raylib.BLANK);
+            DrawMap();
             player.Draw();
             Raylib.EndDrawing();
         }
 
         void DrawMap()
         {
-            foreach(var layer in map.layers)
+            for (int i = 0; i < map.layers.Count; i++)
+            //oreach(var layer in map.layers)
             {
                 for (int row = 0; row < map.height; row++)
                 {
                     for (int col = 0; col < map.width; col++)
                     {
-                        int tileId = layer.data[row * layer.width + col];
+                        int tileId = map.layers[i].data[row * map.layers[i].width + col];
                         int x = col * map.tilewidth;
                         int y = row * map.tileheight;
-                        //DrawTile(x, y, tileId);
+                        DrawTile(i, x, y, tileId);
                     }
                 }
             }
+        }
+
+        void DrawTile(int index, int x, int y, int TileId)
+        {
+            //Tiled aloittaa laskennan yhdestä.
+            TileId -= 1;
+            int tilesPerRow = map.tilesetFiles[index].imagewidth / map.tilewidth;
+            //Console.WriteLine((float)TileId / (float)tilesPerRow);
+            float rowf = MathF.Floor(TileId / tilesPerRow);
+
+            int row = Convert.ToInt32(rowf);
+            int column = TileId % tilesPerRow;
+
+            int u = column * map.tilewidth;
+            int v = row * map.tileheight;
+
+            Raylib.DrawTextureRec(tileSets[index], new Rectangle(u, v, map.tilewidth, map.tileheight), new Vector2(x, y), Raylib.WHITE);
+
         }
 
 
